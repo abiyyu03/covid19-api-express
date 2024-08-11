0a158004b10834d4163d3cad5d845fc4ccebb310
dhonyimam <dhony.imam@moladin.com>
2024-08-11 14:30:25 +0700

    feat: upgrade mysql lib and describe port

--- config/database.js
+++ config/database.js
@@ -1,11 +1,11 @@
 // import mysql
-const mysql = require("mysql");
+const mysql = require("mysql2");
 
 // import dotenv dan menjalankan method config
 require("dotenv").config();
 
 // destructing object process.env
-const { DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE } = process.env;
+const { DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD, DB_DATABASE } = process.env;
 
 /**
  * Membuat koneksi database menggunakan method createConnection
@@ -13,9 +13,10 @@ const { DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE } = process.env;
  */
 const db = mysql.createConnection({
   host: DB_HOST,
+  port: DB_PORT,
   user: DB_USERNAME,
   password: DB_PASSWORD,
-  database: DB_DATABASE,
+  database: DB_DATABASE
 });
 
 /**
