b55f60653f71d8401420b4b66535a6baef2cb1fc
DhonyImamSaputra <dhony.imamsaputra@gmail.com>
2024-09-06 23:25:27 +0700

    feat: add new cases for unit tests

--- tests/controllers/PatientController.test.js
+++ tests/controllers/PatientController.test.js
@@ -4,9 +4,11 @@ import Patient from '@models/Patient'; // Import the original module for mocking
 // Mock the Patient module
 jest.mock('@model/Patient', () => {
   const mockMethodAll = jest.fn();
-        
+  const mockMethodCreate = jest.fn();
+
   return {
     all: mockMethodAll,
+    create: mockMethodCreate,
   }
 });
 
@@ -53,6 +55,33 @@ describe('Patient Controller Test | Controller Test', () => {
       message:"Data is empty",
       statusCode:200
     });
-});
+  });
 
+  test('should succeess create a new data', async () => {
+    const request = {
+      "name": "bima",
+      "phone": "628457126147",
+      "address": "Jl Akamai No 72 Jakarta Selatan",
+      "status": "admitted",
+      "in_date_at": "2024-09-01",
+      "out_date_at": "2024-09-06",
+      "timestamp": "2024-09-05T23:33:13.090"
+    };
+
+    const response = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+    // Mock `Patient.create` for this specific test case
+    Patient.create.mockResolvedValueOnce(request);
+    
+    await patientController.store({body: request}, response);
+    
+    expect(response.status).toHaveBeenCalledWith(201);
+    expect(response.json).toHaveBeenCalledWith({
+      message:"Resource is added successfully",
+      statusCode:201,
+      data: request,
+    });
+  });
 });
